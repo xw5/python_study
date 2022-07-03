be1ecd4032e2b9c40213602e1146668a5a9c7a7c
xiewu <xiewu@ygsoft.com>
2022-07-03 10:13:27 +0800

    尝试文字转语音

--- 365news/txtToAudioModule.py
+++ 365news/txtToAudioModule.py
@@ -2,9 +2,9 @@ import pyttsx3
 
 def textToAudio(text, name):
     engine = pyttsx3.init()
-    rate = engine.getProperty('rate')
+    # rate = engine.getProperty('rate')
     engine.setProperty('rate', 150)
-    volume = engine.getProperty('volume')
+    # volume = engine.getProperty('volume')
     engine.setProperty('volume', 1.0)
     engine.save_to_file(text, '{}.mp3'.format(name))
     engine.runAndWait()
