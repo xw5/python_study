de4917f9a7c8edb4bebeced0ecbbd7195c471612
xiewu <xiewu@ygsoft.com>
2022-07-11 20:45:57 +0800

    尝试网页内容转图片

--- 365news/daynewsSelenium.py
+++ 365news/daynewsSelenium.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
+
 browser = webdriver.Chrome(executable_path=r'E:\study\python\python_reptile\selenium_pro\chromedriver_win32\chromedriver.exe')
 
 browser.get("https://www.163.com/dy/media/T1603594732083.html")
@@ -20,4 +21,3 @@ browser.get("https://voice.baidu.com/act/newpneumonia/newpneumonia/?from=osari_a
 
 element_xg = browser.find_element(By.CLASS_NAME, 'VirusSummarySix_1-54-1_ZRHUKw')
 element_xg.screenshot('xg.png')
-
