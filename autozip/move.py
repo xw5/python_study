f0ab9c4188a71dcbc7310c84cdcc411ecfdc846e
xiewu <xiewu@ygsoft.com>
2022-07-26 15:13:52 +0800

    python按代码行数按文件提取文件

---
+++ autozip/move.py
@@ -0,0 +1,3 @@
+import shutil
+
+shutil.move(r'D:\work\workspace\microLearning_web\src1\manageProject\learningModule\trainClassManage\components\TrainClassCategoryNewChildLevelDialog.vue', r'E:\study\python\python_reptile\autozip\TrainClassCategoryNewChildLevelDialog.vue')
