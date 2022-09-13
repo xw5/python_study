aac00d8aa0acdfb5ad0d32a5ed8805ee3a9fa363
xiewu <xiewu@ygsoft.com>
2022-09-13 11:36:57 +0800

    提交新代码

--- npmlibcheck/runfun.py
+++ npmlibcheck/runfun.py
@@ -13,19 +13,21 @@ def otherInput(proPath, sht, dd, index, keys):
     except:
         sht[index, 1].value = '访问出错'
 
-def statisticsAction(proPath, keys):
+def statisticsAction(proPath, outPath, keys, progressBack, uiName):
     # 读取文件数据
     with open(f'{proPath}/package.json', "r", encoding='utf-8') as f:
         row_data = json.load(f)
     proName = proPath.split('/')[-1]
     app = xw.App(visible=True, add_book=False)  # 启动Excel程序
     workbook = app.books.add()  # 新建工作簿
-    workbook.save(f'./{proName}.xlsx')  # 保存工作簿
+    workbook.save(f'{outPath}/{proName}.xlsx')  # 保存工作簿
     workbook.close()  # 关闭工作簿
     app.quit()
-    wb = xw.Book(f'./{proName}.xlsx')
+    wb = xw.Book(f'{outPath}/{proName}.xlsx')
     sht = wb.sheets['Sheet1']
     index = 0
+    total = len(row_data['devDependencies']) + len(row_data['dependencies'])
+    progress = str(index / total * 100) + '%'
     # 读取每一条json数据
     for d in row_data:
         if d == 'devDependencies':
@@ -33,10 +35,13 @@ def statisticsAction(proPath, keys):
                 sht[index, 0].value = dd
                 otherInput(proPath, sht, dd, index, keys)
                 index = index + 1
+                progress = str(index / total * 100) + '%'
+                progressBack(uiName, progress)
         if d == 'dependencies':
             for dd in row_data[d]:
                 sht[index, 0].value = dd
                 otherInput(proPath, sht, dd, index, keys)
                 index = index + 1
-
+                progress = str(index / total * 100) + '%'
+                progressBack(uiName, progress)
 
--- tkintergui/npmpkgstatistics/npmpkgstatistics_cmd.py
+++ tkintergui/npmpkgstatistics/npmpkgstatistics_cmd.py
@@ -10,12 +10,32 @@ import Fun
 ElementBGArray={}  
 ElementBGArray_Resize={} 
 ElementBGArray_IM={} 
-
+from runfun import statisticsAction
+def progressAction(uiName, value):
+    # 进度更新
+    Fun.SetTKAttrib(uiName, 'Progress_13', 'value', int(value))
+def successBack(uiName, libs):
+    # 完成回调
+    Fun.SetText(uiName, 'Text_10', ('\n').join(libs))
 def Form_1_onLoad(uiName):
+    # 初始化
     Fun.SetText(uiName, 'Text_4', 'version\nlicense\nhomepage')
+    Fun.SetTKAttrib(uiName, 'Progress_13', 'value', 0)
 def Button_3_onCommand(uiName,widgetName):
+    # 设置项目目录
     proDir = tkinter.filedialog.askdirectory()
     Fun.SetText(uiName, 'Entry_2', proDir)
+def Button_12_onCommand(uiName,widgetName):
+    # 设置输出目录
+    outDir = tkinter.filedialog.askdirectory()
+    Fun.SetText(uiName, 'Entry_11', outDir)
 def Button_9_onCommand(uiName,widgetName):
-    print('导出到excel')
+    # 开始运行
+    print('run0')
+    proDir = Fun.GetText(uiName, 'Entry_2')
+    outDir = Fun.GetText(uiName, 'Entry_11')
+    keysStr = Fun.GetText(uiName, 'Text_4')
+    keys = keysStr.split('\n')[0:-1]
+    print('run1', proDir, outDir)
+    statisticsAction(proDir, outDir, keys, progressAction, successBack, uiName)
 
